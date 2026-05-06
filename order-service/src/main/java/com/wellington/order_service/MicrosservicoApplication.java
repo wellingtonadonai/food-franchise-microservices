4dc8776649dfbee31896184bf1cc1e0e44e5c721
wellington <wellington.h.s.oliveira@hotmail.com>
2026-05-06 17:14:49 -0300

    arquivo atualizado

--- order-service/src/main/java/com/wellington/order_service/MicrosservicoApplication.java
+++ order-service/src/main/java/com/wellington/order_service/MicrosservicoApplication.java
@@ -2,8 +2,10 @@ package com.wellington.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.wellington.order_service.repository")
 public class MicrosservicoApplication {
 
 	public static void main(String[] args) {
